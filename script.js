cb84023e9e8cc86e54f8026a97779b8c2241b048
Mubarak <akinsolamubarak1996@gmail.com>
2020-06-03 23:20:25 +0100

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const submit = document.getElementById("submit"),
   search = document.getElementById("search"),
   random = document.getElementById("random"),
-  resultHeading = document.querySelector(".result-heading"),
+  resultHeading = document.getElementById("result-heading"),
   mealEl = document.getElementById("meals"),
   singleMealEl = document.getElementById("single-meal");
 
@@ -16,26 +16,26 @@ function searchMeal(e) {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        resultHeading.innerHTML = `<h2>Search results for '${term}':<h2>`;
+        resultHeading.innerHTML = `<h2>Search results for '${term.toUpperCase()}':<h2>`;
         if (data.meals === null) {
-          resultHeading.innerHTML = `<h2>Nothing Found for '${term}'<h2>`;
+          resultHeading.innerHTML = `<h2>Nothing Found for '${term.toUpperCase()}'<h2>`;
+          mealEl.innerHTML = "";
         } else {
           mealEl.innerHTML = data.meals
-            .map((meal) => {
-              return `<div class='meal'>
-            <img src ='${meal.strMealThumb}'  alt='${meal.strMeal}'/ >
+            .map(
+              (meal) =>
+                `<div class='meal'>
+            <img src ='${meal.strMealThumb}' alt='${meal.strMeal}'/ >
             <div class='meal-info' data-mealID ='${meal.idMeal}'>
             <h3>${meal.strMeal}</h3>
          </div>
         </div>
-            `;
-            })
+            `
+            )
             .join("");
         }
       });
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
+    //clear search text.
     search.value = "";
   } else {
     const div = document.createElement("div");
@@ -50,9 +50,90 @@ function searchMeal(e) {
 
 function timeOut() {
   setTimeout(() => {
-    window.location.reload(true);
+    clearAlert();
+    // window.location.reload(true);
   }, 2000);
+}
+
+function clearAlert() {
+  const currentAlert = document.querySelector(".alert");
+  if (currentAlert) {
+    currentAlert.remove();
+  }
+}
+
+//fetch meal by id function
+function getMealById(meal) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      addMealToDom(meal);
+    });
+}
+
+function addMealToDom(meal) {
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+  singleMealEl.innerHTML = `
+  <div class='single-meal'>
+  <h1>${meal.strMeal}</h1>
+  <img src='${meal.strMealThumb}' alt ='${meal.strMeal}'/>
+  <div class='single-meal-info'>
+${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+${meal.strCategory ? `<p>${meal.strArea}</p>` : ""}
+  </div>
+
+  <div class='main'>
+  <p>${meal.strInstructions}</p>
+  <h2>Ingredients</h2>
+  <ol>
+  ${ingredients
+    .map(
+      (ingre) => `
+    <li>${ingre}</li>
+  `
+    )
+    .join("")}
+  </ol>
+  </div>
+  </div>
+  `;
+}
+
+function getRandomMeal() {
+  mealEl.innerHTML = "";
+  resultHeading.innerHTML = "";
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      addMealToDom(meal);
+    });
 }
 
 //Event listeners
 submit.addEventListener("submit", searchMeal);
+random.addEventListener("click", getRandomMeal);
+mealEl.addEventListener("click", (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains("meal-info");
+    } else {
+      return false;
+    }
+  });
+
+  if (mealInfo) {
+    const mealId = mealInfo.getAttribute("data-mealid");
+    getMealById(mealId);
+  }
+});
